7a2683c2555b779000ee825fb44c76d6364fb3ec
shanmukheswarikondeti <eswarikondeti@gmail.com>
2024-02-29 11:49:09 +0530

    Add Header and Home components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,38 @@
-import {Component} from 'react'
+import { FaBook, FaShoppingBag } from "react-icons/fa";
 
-class Header extends Component{
-    render(){
+import './index.css'
+
+const Header=()=>{
+    
         return(
-            <div>
-                Header
-            </div>
+            <nav className="header-container">
+      <div className="header-content-container">
+        
+          <div className="logo-container">
+            <div className="logo">B</div>
+            BOOKSTORE
+          </div>
+        
+
+        <ul className="nav-items-container">
+          <li className="icon-container">
+            
+              <FaBook className="icon" />
+              <span className="icon-name">Book List</span>
+           
+          </li>
+          <li className="icon-container">
+            
+              <FaShoppingBag className="icon" />
+              <span className="icon-name">Cart</span>
+              
+            
+          </li>
+        </ul>
+      </div>
+    </nav>
         )
-    }
+    
 
 }
 export default Header;
