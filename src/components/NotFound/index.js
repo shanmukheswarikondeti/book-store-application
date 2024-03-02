cbc40d2f2b38e168543565ecd3658f96aaf20180
shanmukheswarikondeti <eswarikondeti@gmail.com>
2024-03-02 17:50:51 +0530

    Add NotFound component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,11 +1,18 @@
 import {Component} from 'react'
+import Header from '../Header'
+
+import './index.css'
 
 class NotFound extends Component{
     render(){
         return(
-            <div>
+            <>
+               <Header/>
+               <div className="not-found-container">
+                   <h1 className="not-fount-text">Page Not Found</h1>
+               </div>
                 NotFound
-            </div>
+            </>
         )
     }
 
