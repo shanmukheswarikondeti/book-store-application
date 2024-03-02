088683cf034a55a96f9fa2c01114d3ec563bc363
shanmukheswarikondeti <eswarikondeti@gmail.com>
2024-03-02 17:11:12 +0530

    Add UserDetailsForm to Checkout component

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,43 +1,108 @@
 import {Component} from 'react'
 import {Link} from 'react-router-dom'
+import UserDetailsForm from '../UserDetailsForm'
+import CartContext from '../../context/CartContext'
 
 import './index.css';
 
-class Checkout extends Component{
-    
-    renderOrderSuccessMessage=()=>{
-        return(
-            <div className="success-message-container">
-                <h1 className="order-success-message">Your Order is Placed</h1>
-                <Link to="/" className="nav-link">
-                    <button className="home-button">Go to Home</button>
-                </Link>
+class Checkout extends Component {
+    state = { is_order_placed: false };
+  
+    renderCartItems = () => (
+      <CartContext.Consumer>
+        {value => {
+          const { cartList } = value;
+  
+          const getTotalCartPrice = () => {
+            let totalCartPrice = 0;
+            cartList.forEach((eachcartItem) => {
+              const price = parseFloat(eachcartItem.price.slice(1));
+              totalCartPrice += price;
+            });
+            return totalCartPrice;
+          };
+          const cartPrice = getTotalCartPrice();
+          return (
+            <div className="checkout-cart-items-container">
+              <h1 className="order-summary-heading"> Order Summary</h1>
+              <ul className="checkout-cart-list-container">
+                {cartList.map((eachCartItem) => {
+                  return (
+                    <li className="checkout-cart-item-container">
+                      <div className="checkout-image-container">
+                        <img
+                          src={eachCartItem.image}
+                          alt={eachCartItem.title}
+                          className="checkout-cart-image"
+                        />
+                        <p className="checkout-cart-text">{eachCartItem.title}</p>
+                      </div>
+                      <p className="checkout-cart-text">{eachCartItem.price}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="total-container">
+                <p className="total-text">Total</p>
+                <p className="total-price">${cartPrice}</p>
+              </div>
             </div>
-        );
-    };
-    
-    render(){
-        return(
+          );
+        }}
+      </CartContext.Consumer>
+    );
+  
+    renderCartAndForm = () => (
+      <CartContext.Consumer>
+        {value => {
+          const onFormSubmitSuccess = () => {
+            this.setState({ isOrderPlaced: true });
+            
+          };
+  
+          return (
             <>
-               <div className="header-without-links-container">
-                    <div className="header-content-container">
-                        <div className="logo-container">
-                            <div className="logo">B</div>
-                                BOOKSTORE
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="checkout-container">
-                        <div className="checkout-content-container">
-                            {this.renderOrderSuccessMessage()}
-                        </div>
-                    </div>
-                
-                
+              {this.renderCartItems()}
+              <UserDetailsForm onFormSuccess={onFormSubmitSuccess} />
             </>
-        )
+          );
+        }}
+      </CartContext.Consumer>
+    );
+  
+    renderOrderSuccessMessage = () => {
+      return (
+        <div className="success-message-container">
+          <h1 className="order-success-message"> Your Order is Placed</h1>
+          <Link to="/" className="nav-link">
+            <button className="home-button">Go to Home</button>
+          </Link>
+        </div>
+      );
+    };
+  
+    render() {
+      const { isOrderPlaced } = this.state;
+      return (
+        <>
+          <div className="header-without-links-container">
+            <div className="header-content-container">
+              <div className="logo-container">
+                <div className="logo">B</div>
+                BOOKSTORE
+              </div>
+            </div>
+          </div>
+          <div className="checkout-container">
+            <div className="checkout-content-container">
+              {isOrderPlaced
+                ? this.renderOrderSuccessMessage()
+                : this.renderCartAndForm()}
+            </div>
+          </div>
+        </>
+      );
     }
-
-}
-export default Checkout;
+  }
+  
+  export default Checkout;
---
+++ src/components/UserDetailsForm/index.js
@@ -0,0 +1,76 @@
+import {Component} from 'react';
+
+import './index.css'
+
+class UserDetailsForm extends Component{
+    state={name:"",address:"",email:"",phone:"",showErrorMessage:false};
+
+    isInputsValid=()=>{
+        const {name,address,email,phone}=this.state;
+        const userDetails={name,address,email,phone};
+
+        const isValidPhone=(phone)=>Number(phone) && phone.length===10;
+        const isNotEmpty=(value)=>value.length>0;
+
+        let isValid=true;
+        for (const[property,value] of Object.entries(userDetails)){
+            if(property==="phone" && !isValidPhone(value)){
+                isValid=false;
+                break;
+            }
+
+            if(property!=="phone" && !isNotEmpty(value)){
+                isValid=false;
+            }
+        }
+        return isValid;
+    };
+
+    onSubmitForm=(event)=>{
+        const {onFormSuccess}=this.props;
+        event.preventDefault();
+        const isInputsValid=this.isInputsValid();
+        if(isInputsValid){
+            onFormSuccess();
+        }
+        else{
+            this.setState({showErrorMessage:true});
+        }
+    };
+
+    onChangeNameInput=(event)=>{
+        this.setState({name: event.target.value});
+    };
+
+    onChangeAddressInput=(event)=>{
+        this.setState({address: event.target.value});
+    };
+
+    onChangeEmailInput=(event)=>{
+        this.setState({email: event.target.value});
+    };
+
+    onChangePhoneInput=(event)=>{
+        this.setState({phone: event.target.value});
+    };
+
+    render(){
+        const {showErrorMessage}=this.state;
+        return(
+            <form className="user-details-form-container" onSubmit={this.onSubmitForm}>
+                <h1 className="user-details-form-heading">Shopping Address</h1>
+                <input type="text" placeholder="Name" className="user-input" required onChange={this.onChangeNameInput}/>
+                <textarea placeholder="Address" className="user-input" required onChange={this.onChangeAddressInput}/>
+                <input type="email" placeholder="Email" className="user-input" required onChange={this.onChangeEmailInput}/>
+                <input type="tel" placeholder="Phone" className="user-input" onChange={this.onChangePhoneInput}/>
+                <button className="place-order-button">Place Order</button>
+                <p className="shipping-note">Only Cash on Delivery is Available</p>
+                {showErrorMessage && (
+                    <p className="error-message">Please enter valid details</p>
+                )}
+            </form>
+        )
+    }
+}
+
+export default UserDetailsForm
